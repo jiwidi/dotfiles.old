b7ff1f22d1598cc65a094c49a6666c62b08ba296
Jaime Ferrando <fhjaime96@gmail.com>
2020-05-07 14:48:54 +0200

    Added more oh my zsh install files

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+_install zsh
+
+# oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+# change shell
+chsh zsh
+
+# plugins and theme
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+
+# link zshrc file
+ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
