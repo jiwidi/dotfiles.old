9f9eec82605b3a632d85389e3852bbe58ee74d1b
Jaime Ferrando <fhjaime96@gmail.com>
2020-05-07 14:19:12 +0200

    vscode install change

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-<<<<<<< HEAD
-=======
 echo "Installing vscode"
->>>>>>> 798535d4f1bf25642fe5a81665c05a4a522a0356
 if command -v code >/dev/null; then
 	if [ "$(uname -s)" = "Darwin" ]; then
 		VSCODE_HOME="$HOME/Library/Application Support/Code"
@@ -19,10 +16,6 @@ if command -v code >/dev/null; then
 		code --install-extension "$module" || true
 	done <"$DOTFILES/vscode/extensions.txt"
 
-<<<<<<< HEAD
 	echo "Updating vscode extensions file"
-=======
-	set -e
->>>>>>> 798535d4f1bf25642fe5a81665c05a4a522a0356
 	code --list-extensions >"$DOTFILES/vscode/extensions.txt"
 fi
