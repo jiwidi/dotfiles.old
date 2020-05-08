3d54fa93d7abe5d4990fa937837753419c8ae2a9
Jaime Ferrando <fhjaime96@gmail.com>
2020-05-08 11:46:18 +0200

    update ohmyzsh installer

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,5 +8,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/cust
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
-
-# link zshrc file
